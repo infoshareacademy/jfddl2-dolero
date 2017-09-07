0e6697d0f7db24ced0f87e303d946330c87a7ed0
piotr-siwek <piotr.siwek.90@gmail.com>
2017-09-07 22:42:34 +0200

    new nav look

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var slideTopBtn = document.getElementById('slideTopBtn');
+
+document.addEventListener('scroll', mouseMoveHandler);
+
+function mouseMoveHandler() {
+
+    clientY = window.scrollY;
+
+    if (clientY > 600)
+        slideTopBtn.className = 'show';
+    else
+        slideTopBtn.className = '';
+    if (clientY > 300)
+        $("nav.navbar-default").addClass("big");
+    else
+        $("nav.navbar-default").removeClass("big");
+}
+
+
+//    jQuery slide to top
+
+
+$("#slideTopBtn, #mainLogo").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+});
+
+// jQuery slide to sections
+$("#additionalInfo").click(function() {
+    $('html, body').animate({
+        scrollTop: $("section.additionalInfo").offset().top
+    }, 500);
+});
+
+$("#info").click(function() {
+    $('html, body').animate({
+        scrollTop: $("section.info").offset().top
+    }, 500);
+});
+
+$("#team").click(function() {
+    $('html, body').animate({
+        scrollTop: $("section.team").offset().top
+    }, 500);
+});
