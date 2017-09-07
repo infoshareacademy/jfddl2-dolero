35c65b3b43e3e7f9fba70d020e17707deedefdc5
Andrzej <andy-26@wp.pl>
2017-09-07 16:45:04 +0200

    zmiana logo

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-$(document).ready(function() {
-    $("#slideshow > div:gt(0)").hide();
+ $(document).ready(function() {
+     $("#slideshow > div:gt(0)").hide();
 
-    setInterval(function () {
-        $('#slideshow > div:first')
-            .fadeOut(2000)
-            .next()
-            .fadeIn(2000)
-            .end()
-            .appendTo('#slideshow');
-    }, 10000);
-});
+     var shownSlide = 0;
+
+     function afterSlide($currentSlide) {
+         console.log($currentSlide.find('li:first-child'));
+     }
+
+     function nextSlide(callback) {
+         var $slideshowItems = $("#slideshow .hero-image");
+         $slideshowItems.fadeOut(4000);
+         var nextSlide = shownSlide + 1;
+
+         $($slideshowItems[nextSlide]).fadeIn(4000);
+         shownSlide = nextSlide;
+         if ((shownSlide + 1) === $slideshowItems.length)
+             shownSlide = -1;
+         callback($($slideshowItems[nextSlide]));
+     }
+
+     setInterval(function () {
+         nextSlide(afterSlide)
+     }, 5000);
+     $('#slideshow li:first-child').addClass('transparent');
+     $('#slideshow li:nth-child(2)').addClass('transparent');
+     $('#slideshow li:nth-last-child(3)').addClass('transparent');
+
+
+
+
+ });
+
+
