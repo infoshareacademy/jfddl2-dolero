20facf019cb94529317e26dee4051647ed2c7ff6
IT <IT@MacBook-Air-IT.local>
2017-09-20 11:56:50 -0700

    generate arrow added

--- js/crossbow.js
+++ js/crossbow.js
@@ -17,52 +17,48 @@ document.addEventListener('mousemove', function (event) {
     var screenHeight = window.innerHeight;
     var triangleHeight = screenHeight - mouseY;
     var triangleWidth = mouseX - centerScreen;
-    console.log('mouseY: ', mouseY, "mouseX: ", mouseX, "centerScreen", centerScreen, "screenHigh", screenHeight, "triangleHeight: ", triangleHeight);
+    // console.log('mouseY: ', mouseY, "mouseX: ", mouseX, "centerScreen", centerScreen, "screenHigh", screenHeight, "triangleHeight: ", triangleHeight);
 
 
     var tang = triangleWidth / triangleHeight;
     // tangens [radians]
-    console.log("tangens w radianach", tang);
+    // console.log("tangens w radianach", tang);
 
 
     var elementDegree = radianToDegree(Math.atan(tang));
 
-    console.log('elementDegree: ', elementDegree);
+    // console.log('elementDegree: ', elementDegree);
     crossbow.style.transform = "rotate(" + elementDegree + "deg)";
-    arrow.style.transform = "rotate(" + elementDegree + "deg)";
+    if (arrow)
+        arrow.style.transform = "rotate(" + elementDegree + "deg)";
 
-    var x1 = mouseX;
-    var y1 = mouseY;
-    var x2 = centerScreen;
-    var y2 = screenHeight;
-    var a = (y2 - y1) / (x2 - x1);
-    var b = y1 - a * x1;
-    var myX = -b / a;
-
-
-    console.log(myX, a, b, x1, y1, x2, y2);
-
-    var rect = arrow.getBoundingClientRect();
-    console.log(rect.top, rect.left);
 
 });
 
 document.addEventListener('click', function (event) {
-
+    var arrow = document.querySelector('.arrow');
+    console.log('arrow', arrow, !arrow);
+    if (!arrow) return;
     var mouseX = event.clientX;
     var mouseY = event.clientY;
 
-    var arrowDiv = document.createElement('img');
-    arrowDiv.className = "arrow";
-    arrowDiv.src = "images/spear_small.svg";
+    setTimeout(function () {
+        arrow.remove();
+    }, 1200);
 
-    (document.querySelector('.arrows')).appendChild(arrowDiv);
-
-    var arrow = document.querySelector('.arrow');
+    setTimeout(function () {
+        var crossbow = document.querySelector('#crossbow');
+        var crossbowTransformProp = crossbow.style.transform;
+        var arrowDiv = document.createElement('img');
+        arrowDiv.className = "arrow";
+        arrowDiv.src = "images/spear_small.svg";
+        arrowDiv.style.transform = crossbowTransformProp;
+        (document.querySelector('.arrows')).appendChild(arrowDiv);
+    }, 1500);
 
 
     var rect = arrow.getBoundingClientRect();
-    console.log(rect.top, rect.right, rect.bottom, rect.left);
+    // console.log(rect.top, rect.right, rect.bottom, rect.left);
 
     var x1 = mouseX;
     var y1 = mouseY;
@@ -70,16 +66,17 @@ document.addEventListener('click', function (event) {
     var y2 = rect.top;
     var a = (y2 - y1) / (x2 - x1);
     var b = y1 - a * x1;
-    var myX = -b/a;
+    var myX = -b / a;
 
 
-
-    arrow.style.bottom =screenHeight+ 'px';
+    arrow.style.bottom = screenHeight + 'px';
     arrow.style.left = myX + 'px';
     arrow.style.transition = '1s';
-    console.log('/////////////////////////////////');
-    console.log('arrow.style.bottom', arrow.style.bottom, 'mouseX * tang', mouseX * tang);
-    console.log('/////////////////////////////////');
-
+    // console.log('/////////////////////////////////');
+    // console.log('arrow.style.bottom', arrow.style.bottom, 'mouseX * tang', mouseX * tang);
+    // console.log('/////////////////////////////////');
 
 });
+
+
+
