6d3184c5d99c9e7d47e0f4cd9b1d3dd56e84a1e0
mopacewicz <31477168+mopacewicz@users.noreply.github.com>
2017-09-29 13:57:43 +0200

    instruction

---
+++ js/instruction.js
@@ -0,0 +1,10 @@
+$('#product-modal').on ('hidden.bs.modal', function (e) {
+    alert('Modal is just hidden!');
+});
+//     backdrop: true,
+//     keyboard: true,
+//     show: false,
+//     remote: true,
+// };
+// loaded.bs.modal
+
